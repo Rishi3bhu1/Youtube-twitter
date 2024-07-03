c52fc3990992d908579d5f9caa59e26d3653291c
Rishi Tribhuwan <rishi.m.tribhuwan@gmail.com>
2024-07-03 11:18:02 +0530

    add register contoller and bug fixes

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -15,9 +15,11 @@ const uploadOnCloudinary = async(localFilePath)=>{
         const response = await cloudinary.uploader.upload(localFilePath,{resource_type:"auto"})
         //file upload successfully
         console.log("File has been uploaded successfully",response.url)
-        return response
+        return response;
     } catch (error) {
         fs.unlinkSync(localFilePath)//remove the locally saved temporary file as the upload operation got failed
         return null
     }
 }
+
+export {uploadOnCloudinary}
